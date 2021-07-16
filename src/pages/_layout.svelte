3ead4eb49951afeeb7d3f8b6807352a610f6e6d2
GHOST <ghostdevbusiness@gmail.com>
2021-07-16 22:04:28 +0100

    feat: wrapper

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,13 @@
+<div class="wrapper">
+    <slot />
+</div>
+
+<style lang="scss">
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+
+        padding: 16px;
+    }
+</style>
