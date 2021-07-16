1dfc0376c9f22df4995f60df9069ba5c94ac3e38
GHOST <ghostdevbusiness@gmail.com>
2021-07-17 00:58:33 +0100

    feat: raw page

---
+++ src/pages/tasks/raw.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { tasks, clearTasks } from './tasks';
+</script>
+
+<div class="row">
+    <a href="/tasks" class="button">Back</a>
+    <button on:click={clearTasks}>Clear</button>
+</div>
+
+<pre>
+    {JSON.stringify($tasks, null, 4)}
+</pre>
