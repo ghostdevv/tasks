68751139e6dea462dec57fa003ca58811c11d4cd
GHOST <ghostdevbusiness@gmail.com>
2021-07-17 01:06:05 +0100

    feat: more logic

--- src/pages/tasks/tasks.js
+++ src/pages/tasks/tasks.js
@@ -1,5 +1,6 @@
 import { writable } from 'svelte-local-storage-store';
 import { v4 as uuid } from '@lukeed/uuid';
+import { get } from 'svelte/store';
 
 export const tasks = writable('tasks', []);
 
@@ -12,5 +13,18 @@ export const createTask = (name) =>
         },
         ...tasks,
     ]);
+
+export const updateTask = (id, name) =>
+    tasks.update((tasks) =>
+        tasks.map((t) => {
+            if (t.id == id) {
+                t.name = name;
+            }
+
+            return t;
+        }),
+    );
+
+export const getTask = (id) => get(tasks).find((t) => t.id == id);
 
 export const clearTasks = () => tasks.set([]);
