5df90aee34e876a203fe6c23515a0eaf8b71e9e2
GHOST <ghostdevbusiness@gmail.com>
2021-07-30 15:24:17 +0100

    feat: dev mode

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,14 @@
 <script>
-    import { Router } from '@roxi/routify';
     import { routes } from '../.routify/routes';
+    import { Router } from '@roxi/routify';
+    import { hotkeys } from 'svelte-hotkeys';
+    import { dev } from '@/config.js';
 </script>
 
 <Router {routes} />
+
+<svelte:window
+    use:hotkeys={{
+        keys: 'ctrl+alt+d',
+        handler: () => ($dev = !$dev),
+    }} />
