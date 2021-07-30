427c8ef288b001906bf194492e3baad666a2ef11
GHOST <ghostdevbusiness@gmail.com>
2021-07-30 17:00:46 +0100

    refactor: task component

---
+++ src/components/Task.svelte
@@ -0,0 +1,29 @@
+<script>
+    import DeleteButton from '@/components/DeleteButton.svelte';
+    import TaskModal from '@/components/TaskModal.svelte';
+    import { getTask, deleteTask } from '@/tasks.js';
+    import { fly } from 'svelte/transition';
+
+    export let id;
+    export let name;
+    export let i;
+</script>
+
+<TaskModal {id} existing>
+    <card
+        in:fly={{ y: -20, delay: 150 * i }}
+        out:fly|local={{ y: -20 }}
+        slot="activator">
+        <h4 style="font-weight: 400;">{name}</h4>
+
+        <DeleteButton on:click={() => deleteTask(id)} />
+    </card>
+</TaskModal>
+
+<style>
+    card {
+        cursor: pointer;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+</style>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,7 @@
 <script>
-    import DeleteButton from '@/components/DeleteButton.svelte';
-    import { tasks, clearTasks, deleteTask } from '@/tasks.js';
     import TaskModal from '@/components/TaskModal.svelte';
-    import { fly } from 'svelte/transition';
+    import { tasks, clearTasks } from '@/tasks.js';
+    import Task from '@/components/Task.svelte';
 </script>
 
 <div class="row">
@@ -15,28 +14,6 @@
 
 <div class="tasks">
     {#each $tasks as task, i (task.id)}
-        <TaskModal id={task.id} existing>
-            <card
-                in:fly={{ y: -20, delay: 150 * i }}
-                out:fly|local={{ y: -20 }}
-                slot="activator">
-                <h4 style="font-weight: 400;">{task.name}</h4>
-                <DeleteButton on:click={() => deleteTask(task.id)} />
-            </card>
-        </TaskModal>
+        <Task {...task} {i} />
     {/each}
 </div>
-
-<style lang="scss">
-    @use 'style/flow/general';
-
-    .tasks {
-        @include general.column;
-
-        card {
-            cursor: pointer;
-            flex-direction: row;
-            justify-content: space-between;
-        }
-    }
-</style>
