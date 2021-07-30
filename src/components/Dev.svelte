9b7966cf9eaa08ad6a1f58cdde41804f874e8030
GHOST <ghostdevbusiness@gmail.com>
2021-07-30 18:58:50 +0100

    feat: delay

--- src/components/Dev.svelte
+++ src/components/Dev.svelte
@@ -4,8 +4,13 @@
     import { dev } from '@/config.js';
 
     function addRandomTasks() {
-        for (let x = 0; x < 5; x++)
-            createTask(Date.now(), { completed: Math.random() >= 0.5 });
+        for (let x = 0; x < 5; x++) {
+            setTimeout(
+                () =>
+                    createTask(Date.now(), { completed: Math.random() >= 0.5 }),
+                x * 200,
+            );
+        }
     }
 </script>
 
