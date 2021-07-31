7e0e8b85f73c062a9200c27f9f681265f17a21ff
GHOST <ghostdevbusiness@gmail.com>
2021-07-31 11:07:16 +0100

    fix: spacing

--- src/pages/raw.svelte
+++ src/pages/raw.svelte
@@ -2,11 +2,13 @@
     import { tasks, clearTasks } from '@/tasks';
 </script>
 
-<div class="row">
-    <a href="/" class="button">Back</a>
-    <button on:click={clearTasks}>Clear</button>
-</div>
+<div class="col g16" style="padding: 16px 0px;">
+    <div class="row">
+        <a href="/" class="button">Back</a>
+        <button on:click={clearTasks}>Clear</button>
+    </div>
 
-<pre>
-    {JSON.stringify($tasks, null, 4)}
-</pre>
+    <pre>
+        {JSON.stringify($tasks, null, 4)}
+    </pre>
+</div>
