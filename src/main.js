47b1bad927b4e7e5a98977de1c86ce88956cddde
GHOST <ghostdevbusiness@gmail.com>
2021-08-01 18:55:59 +0100

    refactor: switch to vite-plugin-pwa

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import '../style/global.scss';
+
+import { registerSW } from 'virtual:pwa-register';
 import HMR from '@roxi/routify/hmr';
 import App from './App.svelte';
 
+// Update/register service worker
+registerSW()();
+
+// create svelte app
 const app = HMR(App, { target: document.body }, 'tasks');
 
 export default app;
